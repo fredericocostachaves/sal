feb37ab2718d9f07f7ddd3f7f9116e43c64133c2
Frederico Costa Chaves <fcostachaves@gmail.com>
2026-02-09 22:42:12 -0300

    Implementa fluxo de autenticação com LinkedIn e configuração do OpenAPI para documentação

---
+++ src/main/java/com/sal/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.sal.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("SAL API")
+                        .version("0.0.1")
+                        .description("API para integração com LinkedIn e outras funcionalidades."));
+    }
+}
